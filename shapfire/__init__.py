56662cf481c42569b7a9f77e3877c8b014488e7c
NicklasXYZ <nsa200293@live.dk>
2022-06-13 11:44:54 +0200

    Add function to lib

--- shapfire/__init__.py
+++ shapfire/__init__.py
@@ -3,5 +3,9 @@
 name = "shapfire"
 
 from shapfire.plotting import plot_roc_curve  # noqa
-from shapfire.shapfire import RefitHelper, ShapFire  # noqa
+from shapfire.shapfire import (  # noqa
+    RefitHelper,
+    ShapFire,
+    hyperparameter_search_helper,
+)
 from shapfire.utils import associations  # noqa
